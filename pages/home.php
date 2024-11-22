49908cde3a27f573ebdcbdbf32cc819dab8a639e
LuccasNico <luccasdnico@hotmail.com>
2024-11-22 16:45:43 -0300

    home funcional

--- pages/home.php
+++ pages/home.php
@@ -3,11 +3,61 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Home</title>
 </head>
 <body>
-    <?php 
-        echo "teste";
+    <h1>Bem vindo ao editor de tabela</h1>
+    <h3>Selecione a tabela a ser editada</h3>
+    <label for="Tabelas">Selecione a tabela</label>
+    <form method="post" action="">
+        <select name="Tabelas">
+            <option value="table1">Autores</option>
+            <option value="table2">Classificação Autores</option>
+            <option value="table3">Autores</option>
+            <option value="table4">Autores</option>
+            <option value="table5">Autores</option>
+            <option value="table6">Autores</option>
+        </select>
+        <input type="submit" value="enviar">
+    </form>
+    <?php
+        error_reporting(E_ALL & ~E_WARNING);
+        $valor = $_POST['Tabelas'];
+        
+        switch ($valor) {
+            case 'table1':
+                header("Location: table1.php");
+                exit();
+                break;
+            
+            case 'table2':
+                header("Location: table1.php");
+                exit();
+                break;
+            
+            case 'table3':
+                header("Location: table1.php");
+                exit();
+                break;
+            
+            case 'table4':
+                header("Location: table1.php");
+                exit();
+                break;
+            
+            case 'table5':
+                header("Location: table1.php");
+                exit();
+                break;
+            
+            case 'table6':
+                header("Location: table1.php");
+                exit();
+                break;
+            
+            default:
+                break;
+        }
     ?>
 </body>
 </html>
--- pages/table1.php
+++ pages/table1.php
@@ -3,11 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Autores</title>
 </head>
 <body>
+    <h1>Autores</h1>
     <?php 
-    echo "aaaaaaaaaaaaaa";
+        echo "aaaaaaaaaaaaaa";
     ?>
 </body>
 </html>
