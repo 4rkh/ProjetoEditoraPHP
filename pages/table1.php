85d827b181b03e6ac53311bff5f5495075ff8c82
LuccasNico <luccasdnico@hotmail.com>
2024-11-29 14:28:29 -0300

    Update table1.php

--- pages/table1.php
+++ pages/table1.php
@@ -1,14 +1,30 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Autores</title>
 </head>
+
 <body>
     <h1>Autores</h1>
-    <?php 
-        echo "aaaaaaaaaaaaaa";
+    <?php
+    $host = 'localhost'; 
+    $dbname = 'db editora'; 
+    $username = 'user'; 
+    $password = 'user'; 
+
+    try {
+       
+        $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        echo "Conexão bem-sucedida ao banco de dados!";
+    } catch (PDOException $e) {
+        echo "Erro na conexão: " . $e->getMessage();
+    }
     ?>
 </body>
+
 </html>
