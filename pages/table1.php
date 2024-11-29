efb695eb1fd701d3b99308eecd241c1799d2cf5e
Arthur Rodrigues <141776138+4rkh@users.noreply.github.com>
2024-11-29 16:23:33 -0300

    Update table1.php

--- pages/table1.php
+++ pages/table1.php
@@ -12,10 +12,39 @@
     <?php
     include '../classes/Tabela.php';
     class Autores extends Tabela{
-        
+        function deletarLinha($id) {
+            $host = 'localhost'; 
+            $dbname = 'db editora'; 
+            $username = 'user'; 
+            $password = 'user'; 
+    
+            try {
+                $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+                $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+                $query = "DELETE FROM $this->tableName WHERE id_autor = :id";
+                $stmt = $pdo->prepare($query);
+                $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+    
+                if ($stmt->execute()) {
+                    echo "Registro com ID $id foi deletado com sucesso.";
+                } else {
+                    echo "Erro ao deletar o registro.";
+                }
+            } catch (PDOException $e) {
+                echo "Erro na conexão: " . $e->getMessage();
+            }
+        }
     }
+        
     $autores = new Autores("autores");
     $autores->exibirTabela();
+       // Lógica para processar a exclusão
+       if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['delete_id'])) {
+        $id = intval($_POST['delete_id']);
+        $tabela = new Autores(''); // Substitua pelo nome da tabela
+        $tabela->deletarLinha($id);
+    }
     ?>
 </body>
 
